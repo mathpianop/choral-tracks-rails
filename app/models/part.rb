3720a923ec63899a63b176580f1391f2f47d81c8
Paul Cline <mathpianop@gmail.com>
2021-06-24 20:33:06 -0500

    Added presence validations for Song and Part models

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,5 @@
 class Part < ApplicationRecord
   belongs_to :song, counter_cache: true
+  validates :song, presence: true
+  validates :initial, :recording, :name, :pitch_order, :public_id, presence: true
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ApplicationRecord
   has_many :parts, dependent: :destroy
+  validates :title, :parts_promised, presence: true
 end
