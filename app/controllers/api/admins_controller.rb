6908ab3a904f29128ebecb3e1628c53bd4d070ac
Paul Cline <mathpianop@gmail.com>
2021-06-28 16:54:43 -0500

    Got rid debugging print statements

--- app/controllers/api/admins_controller.rb
+++ app/controllers/api/admins_controller.rb
@@ -2,12 +2,10 @@ class Api::AdminsController < ApplicationController
   # LOGGING IN
   def login
     @admin = Admin.find_by(username: params[:username])
-    p "Hello"
     if @admin && @admin.authenticate(params[:password])
       token = encode_token({admin_id: @admin.id})
       render json: {admin: @admin, token: token, status: 200}
     else
-      p "Hello"
       render json: {error: "Invalid username or password", status: 401}
     end
   end
