46e245e2221ea1c9333a661a5366316a8b0c7af3
Paul <mathpianop@gmail.com>
2022-10-10 21:29:11 -0500

    Configure hosts in development

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,10 +1,10 @@
 class Part < ApplicationRecord
   belongs_to :song, counter_cache: true
-  before destroy :delete_uploaded_recording
+  before_destroy :delete_uploaded_recording
   validates :song, presence: true
   validates :initial, :recording_url, :name, :pitch_order, :public_id, presence: true
 
-  def delete_uploaded_recording(alt_public_id)
+  def delete_uploaded_recording(alt_public_id = nil)
     public_id = alt_public_id || self.public_id
     Cloudinary::Uploader.destroy(public_id, resource_type: :video)
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,6 +104,4 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.hosts << "choral-tracks-rails.up.railway.app"
-  p "Hello World"
-  config.hosts.clear
 end
